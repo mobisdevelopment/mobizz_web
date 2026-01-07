8c4864e5ce95192fc8672b46136eabc75b92fba9
Dragos Holban <dragos.holban@gmail.com>
2026-01-07 12:54:52 +0200

    Submit contact form using API

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -2,7 +2,7 @@ export const API_CONFIG = {
   BASE_URL: "https://mobizzapp.ro/",
   ENDPOINTS: {
     CONTACT: {
-      SAVE: "api/contact",
+      SAVE: "api/contact_messages",
     },
   },
 } as const;
